952ca1a03e56ebcb3fb33a3cc975aaab04b15953
Thiago <thialbani02@gmail.com>
2024-08-13 10:24:02 -0300

    prepared app for production

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json()); // allow json
 app.use(cookieParser()); // allow cookies
+const __dirname = path.resolve();
 
 app.listen(PORT, () => {
   connectDB();
@@ -19,3 +21,11 @@ app.listen(PORT, () => {
 });
 
 app.use("/api/auth", authRoutes); // auth routes
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  })
+}
